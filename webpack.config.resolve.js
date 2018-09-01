b2a11dd4654635963b0d1c3b27e2358df0be2ee3
Jorge Ramirez <me.mark.two@gmail.com>
2018-08-31 23:59:18 -0400

    Update aliases for better folder structure

--- webpack.config.resolve.js
+++ webpack.config.resolve.js
@@ -7,11 +7,11 @@ module.exports = {
   extensions: ['.js', '.jsx', '.json'],
   modules: [path.join(__dirname, 'app'), 'node_modules'],
   alias: {
+    '@app': path.resolve(__dirname, 'app'),
     '@components': path.resolve(__dirname, 'app/components'),
     '@config': path.resolve(__dirname, 'app/config'),
     '@constants': path.resolve(__dirname, 'app/constants'),
     '@data': path.resolve(__dirname, 'app/data'),
-    '@utils': path.resolve(__dirname, 'app/utils'),
-    '@routes': path.resolve(__dirname, 'app/routes')
+    '@utils': path.resolve(__dirname, 'app/utils')
   }
 };
