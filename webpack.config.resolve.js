0641028b3cc684aab5f8e086f027ddf338709d79
Jorge Ramirez <me.mark.two@gmail.com>
2018-08-31 21:56:37 -0400

    Implement webpack alias

---
+++ webpack.config.resolve.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+/**
+ * Determine the array of extensions that should be used to resolve modules.
+ */
+module.exports = {
+  extensions: ['.js', '.jsx', '.json'],
+  modules: [path.join(__dirname, 'app'), 'node_modules'],
+  alias: {
+    '@components': path.resolve(__dirname, 'app/components'),
+    '@config': path.resolve(__dirname, 'app/config'),
+    '@constants': path.resolve(__dirname, 'app/constants'),
+    '@data': path.resolve(__dirname, 'app/data'),
+    '@utils': path.resolve(__dirname, 'app/utils'),
+    '@routes': path.resolve(__dirname, 'app/routes')
+  }
+};
